44fb0f4afceb0cebcce43c15d0892daf73db7a23
LukasBager <lukasemilbager@protonmail.com>
2026-01-28 11:20:55 +0100

    Added scanner to program for fun

--- src/Lesson01/MyFirstJavaCode.java
+++ src/Lesson01/MyFirstJavaCode.java
@@ -1,10 +1,21 @@
 package Lesson01;
 
+import java.util.Scanner;
+
 public class MyFirstJavaCode {
 
     public static void main(String[] args) {
 
         System.out.println("Goddav du");
+
+        /* HYGGE KODE:
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Skriv dit navn");
+
+        String navn = scanner.next();
+        System.out.println("Hej med dig " + navn);
+        */
 
     }
 
