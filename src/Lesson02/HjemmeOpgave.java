2b16cdfb7ed56c816f67746b68479a3a083392fa
LukasBager <lukasemilbager@protonmail.com>
2026-01-29 18:29:36 +0100

    Completed bonus assignment

--- src/Lesson02/HjemmeOpgave.java
+++ src/Lesson02/HjemmeOpgave.java
@@ -3,9 +3,24 @@ package Lesson02;
 public class HjemmeOpgave {
 
     public static void main(String[] args) {
+
+        // Primary assignment
         System.out.println("Mission \t Year \t Days in Space");
         System.out.println("IRISS \t\t 2015 \t 10");
         System.out.println("Huginn \t\t 2023 \t 180");
+
+        // Bonus assignment
+        System.out.println("  /\\");
+        System.out.println(" /  \\");
+        System.out.println("/____\\");
+        System.out.println("|    |");
+        System.out.println("|ISS |");
+        System.out.println("|____|");
+        System.out.println("  ||");
+        System.out.println("  ||");
+        System.out.println(" /  \\");
+        System.out.println("/____\\");
+
     }
 
 }
