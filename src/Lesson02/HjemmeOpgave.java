38d8e8edc0f9df2442c7b922cf7d38ff3f5267d4
LukasBager <lukasemilbager@protonmail.com>
2026-02-03 08:56:00 +0100

    Optimeret kode

--- src/Lesson02/HjemmeOpgave.java
+++ src/Lesson02/HjemmeOpgave.java
@@ -4,12 +4,19 @@ public class HjemmeOpgave {
 
     public static void main(String[] args) {
 
-        // Primary assignment
+        // Primary assignment - first try
+        /*
         System.out.println("Mission \t Year \t Days in Space");
         System.out.println("IRISS \t\t 2015 \t 10");
         System.out.println("Huginn \t\t 2023 \t 180");
+         */
 
-        System.out.println("\n");
+        // Primary assignment - correct solution
+        System.out.println("Mission\t Year\t Days in Space");
+        System.out.println("IRISS\t 2015\t 10");
+        System.out.println("Huginn\t 2023\t 180");
+
+        System.out.print("\n");
 
         // Bonus assignment
         System.out.println("  /\\");
@@ -22,6 +29,11 @@ public class HjemmeOpgave {
         System.out.println("  ||");
         System.out.println(" /  \\");
         System.out.println("/____\\");
+
+        System.out.print("\n");
+
+        // In one line
+        System.out.println("  /\\\n /  \\\n/____\\\n|    |\n|ISS |\n|____|\n  ||\n  ||\n /  \\\n/____\\");
 
     }
 
